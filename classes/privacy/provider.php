618d28ea7999aa5d867de7cea84a1bd1cb4886b7
Charles Fulton <fultonc@lafayette.edu>
2025-10-15 20:43:43 -0400

    Update provider function

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -31,16 +31,13 @@ namespace local_remote_courses\privacy;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class provider implements \core_privacy\local\metadata\null_provider {
-    // This trait must be included.
-    use \core_privacy\local\legacy_polyfill;
-
     /**
      * Get the language string identifier with the component's language
      * file to explain why this plugin stores no data.
      *
      * @return  string
      */
-    public static function _get_reason() {
+    public static function get_reason(): string {
         return 'privacy:metadata';
     }
 }
